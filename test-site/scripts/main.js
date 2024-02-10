3f520cf786f3b2c45b841f9cf1cc6cb0540ba046
Red <135398446+yw74522@users.noreply.github.com>
2024-02-11 00:39:17 +0800

    Add files via upload

---
+++ test-site/scripts/main.js
@@ -0,0 +1,27 @@
+// 图像切换器
+let myImage = document.querySelector("img");
+
+myImage.onclick = function () {
+  let mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/firefox-icon.png") {
+    myImage.setAttribute("src", "images/road2.jpg");
+  } else {
+    myImage.setAttribute("src", "images/firefox-icon.png");
+  }
+};
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+function setUserName() {
+    let myName = prompt("请输入你的名字。");
+    if (!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem("name", myName);
+      myHeading.textContent = "Mozilla 酷毙了，" + myName;
+    }
+  }
+  
+myButton.onclick = function () {
+    setUserName();
+};
+  
